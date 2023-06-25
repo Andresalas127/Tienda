1e0cfb73c5de777a7baa5e34e8eed6e4f331f547
André <Andr@DESKTOP-FKN5E6N>
2023-06-25 14:47:02 -0600

    Commit con la semana 6

--- src/main/java/com/tienda/service/impl/CategoriaServiceImpl.java
+++ src/main/java/com/tienda/service/impl/CategoriaServiceImpl.java
@@ -1,4 +1,3 @@
-
 package com.tienda.service.impl;
 
 import com.tienda.dao.CategoriaDao;
@@ -10,21 +9,39 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-public class CategoriaServiceImpl implements CategoriaService{
-    
+public class CategoriaServiceImpl implements CategoriaService {
+
     @Autowired
     CategoriaDao categoriaDao;
 
     @Override
-    @Transactional(readOnly =true)
+    @Transactional(readOnly = true)
     public List<Categoria> getCategorias(boolean activos) {
         List<Categoria> categorias = categoriaDao.findAll();
-        
-        if(activos){
+
+        if (activos) {
             categorias.removeIf(x -> !x.isActivo());
         }
-        
+
         return categorias;
     }
-    
+
+    @Override
+    @Transactional(readOnly = true)
+    public Categoria getCategoria(Categoria categoria) {
+        return categoriaDao.findById(categoria.getIdCategoria()).orElse(null);
+    }
+
+    @Override
+    @Transactional
+    public void save(Categoria categoria) {
+        categoriaDao.save(categoria);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Categoria categoria) {
+        categoriaDao.delete(categoria);
+    }
+
 }
